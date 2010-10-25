1a260731ecc557e4ea2180ef2b223492d0a2c821
Jeremy <jdfoote1@gmail.com>
2010-10-24 21:05:09 -0600

    Added the other API

--- KivaTest.py
+++ KivaTest.py
@@ -1,9 +1,11 @@
+# So far, this just grabs the first page of data from each of the searches that we want. The next step is to append the next pages to the list.
+
 import urllib
 import simplejson
-data = urllib.urlopen('http://api.kivaws.org/v1/teams/96/lenders.json').read()
-testdata = simplejson.loads(data)
-l = testdata['lenders']
-for i in range(len(l)):
-	f = l[i]
-	w = i['whereabouts']
-	print 'Location' w
+mormonsJSON = urllib.urlopen('http://api.kivaws.org/v1/teams/96/lenders.json').read() # Get the names of the Kiva Mormons members.
+newLenderSearch = 'http://api.kivaws.org/v1/lenders/search.json?q=Utah'
+newLendersJSON = urllib.urlopen(newLenderSearch).read()
+mormons = simplejson.loads(mormonsJSON)['lenders'] # Gets list of lenders from Kiva Mormons team. 
+newLenders = simplejson.loads(newLendersJSON)['lenders'] # Gets list of new lenders
+for s in newLenders:
+	print s
