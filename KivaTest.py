1fc01c0c7e6fff05b1efb501a9f1738a7e60845b
jdfoote <jdfoote1@gmail.com>
2010-10-26 15:24:39 -0700

    I think this is right.

--- KivaTest.py
+++ KivaTest.py
@@ -6,9 +6,9 @@ import simplejson
 # newLenderSearch = 'http://api.kivaws.org/v1/lenders/search.json?q=' + queryString
 # newLendersJSON = urllib.urlopen(newLenderSearch).read()
 # newLenders = simplejson.loads(newLendersJSON)['lenders'] # Gets list of new lenders
-# mormonsRange = simplejson.loads(urllib.urlopen('http://api.kivaws.org/v1/teams/96/lenders.json').read())['paging']
+mormonsRange = simplejson.loads(urllib.urlopen('http://api.kivaws.org/v1/teams/96/lenders.json').read())['paging']['pages']
 mormons = []
-for i in range(5):
+for i in range(mormonsRange):
 	newJSON = urllib.urlopen('http://api.kivaws.org/v1/teams/96/lenders.json?page=' + str(i+1)).read()
 	new = simplejson.loads(newJSON)['lenders']
 	mormons.extend(new)
