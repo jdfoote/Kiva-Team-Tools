d35085ad5beb55cb8896e8238012c38e4a3fa60f
Jeremy <jdfoote1@gmail.com>
2010-10-27 21:35:51 -0600

    Opens web page for each lender, writes out to last5* files

--- KivaTest.py
+++ KivaTest.py
@@ -1,45 +1,83 @@
+# I think that this is pretty close to being done.
+# To Do:
+# If possible, I would like to look into sending the messages through Python, or at least filling in the message field on the web page, but other than that, we're there. 
+# Need to do some error checking, still. Maybe write some unit tests, but that might be overkill.
+# Should also write out the UIDs that have been messaged, along with a timestamp?
+
 import urllib
 import simplejson
+import webbrowser
 
+#Get the recent lenders
 try:
-    f = open('last5lenders.txt','r+')
-    topFiveUsers = f.read()
-    print topFiveUsers
+    f = open('last5Lenders.txt','r')
+    lastFiveLenders = f.read()
+    print lastFiveLenders
     f.close()
 except IOError:
-    topFiveUsers = ''
+    lastFiveLenders = ''
     print "File doesn't exist"
-newLenderSearch = 'http://api.kivaws.org/v1/lenders/search.json?q=Utah&sort_by=newest'
-newLendersRange = simplejson.loads(urllib.urlopen(newLenderSearch).read())['paging']['pages']
-potentialMembers = []
 
-topFiveUsers = 'JohnC'
+try:
+    f = open('last5Mormons.txt','r')
+    lastFiveMormons = f.read()
+    print lastFiveMormons
+    f.close()
+except IOError:
+    lastFiveMormons = ''
+    print "File doesn't exist"
 
-for i in range(newLendersRange):
-    urlStr = 'http://api.kivaws.org/v1/lenders/search.json?q=Utah&sort_by=newest&page=' + str(i+1)
-    newPotentialMembers = simplejson.loads(urllib.urlopen(urlStr).read())['lenders']
-    for j in newPotentialMembers:
+
+# Get all of the recent lenders
+newLendersRange = simplejson.loads(urllib.urlopen('http://api.kivaws.org/v1/lenders/search.json?q=Utah&sort_by=newest').read())['paging']['pages']
+lenders = []
+i = 1
+while i <= newLendersRange:
+    urlStr = 'http://api.kivaws.org/v1/lenders/search.json?q=Utah&sort_by=newest&page=' + str(i)
+    newLenders = simplejson.loads(urllib.urlopen(urlStr).read())['lenders']
+    for j in newLenders:
         lenderID = j['uid']
-        potentialMembers.append(lenderID)
+	if lenderID not in lastFiveLenders:
+		lenders.append(lenderID)
+		i = i+1
+	else:
+		i = newLendersRange + 1 # This is a hack to end the while loop. I don't know how to end it correctly.
+		break
 
+# Get all of the recent Mormons
 mormonsRange = simplejson.loads(urllib.urlopen('http://api.kivaws.org/v1/teams/96/lenders.json').read())['paging']['pages']
 mormons = []
 i = 1
 while i <= mormonsRange:
-    newMormons = simplejson.loads(urllib.urlopen('http://api.kivaws.org/v1/teams/96/lenders.json?page=' + str(i+1)).read())['lenders']
-    for subdict in newMormons:
-		newLenderID = subdict['uid']        
-		if newLenderID != topFiveUsers:
-			mormons.append(newLenderID)
+    newMormons = simplejson.loads(urllib.urlopen('http://api.kivaws.org/v1/teams/96/lenders.json?page=' + str(i)).read())['lenders']
+    for j in newMormons:
+		mormonID = j['uid']        
+		if mormonID not in lastFiveMormons:
+			mormons.append(mormonID)
 			i = i+1
 		else:
+			i = mormonsRange + 1
 			break
 
-for i in potentialMembers:
+#Compare the recent lenders and the recent Mormons
+for i in lenders:
 		noMatch = True
 		for j in mormons:
-				if i == j:
+				if i != j:
 						noMatch = False
 						break
 		if noMatch == False:
-				print "Potential Member:", i
+			print 'Potential Recruit: ' + i
+#			webbrowser.open("http://www.kiva.org/lender" + i) # This opens a browser window for each lender's page, so that a message can be sent. We can activate this when we are sure everything is working correctly.
+
+# Write the 5 most recent UIDs to the docs
+s = str(lenders[:5])
+f = open('last5Lenders.txt', 'w')
+f.write(s)
+f.close()
+
+s = str(mormons[:5])
+f = open('last5Mormons.txt', 'w')
+f.write(s)
+f.close()
+
